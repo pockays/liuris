4c976b524a0f9d105c5d464e5a5265924cb7637e
liuris <1159419473@qq.com>
2023-03-13 09:09:15 +0800

    小改了一下服务器启动的问题

--- src/server/router.js
+++ src/server/router.js
@@ -4,7 +4,6 @@ let user = require('./API/user')
 
 router.get('/user',user.get)
 router.get('/update',user.update)
-router.get('/user',user.notesget)
-router.get('/update',user.notesupdate)
+
 
 module.exports = router
